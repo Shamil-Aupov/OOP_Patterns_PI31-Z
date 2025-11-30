99a9c76edf157d850e15902779c7e80443ccb10d
Shamil-Aupov <shomaayupof@gmail.com>
2025-11-30 15:02:52 +0500

    added Singleton

---
+++ Singleton.php
@@ -0,0 +1,33 @@
+<?php
+
+class GameSettings {
+    
+    private static $instance = null;
+    
+  
+    public $volume = 50;
+    public $difficulty = 'Normal';
+
+    
+    private function __construct() {}
+
+
+    public static function getInstance(): self {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+
+$settings1 = GameSettings::getInstance();  
+$settings2 = GameSettings::getInstance();
+
+echo ($settings1 === $settings2 ? 'True' : 'False') . "\n";
+
+$settings1->volume = 70;
+$settings1->difficulty = 'Hard';
+
+echo $settings2->volume . "\n";      
+echo $settings2->difficulty . "\n"; 
