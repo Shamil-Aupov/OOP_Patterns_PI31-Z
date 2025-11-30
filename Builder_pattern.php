b31e3c532586049f2a4e9f67891d97c4459db03e
Shamil-Aupov <shomaayupof@gmail.com>
2025-11-30 14:51:16 +0500

    Added Builder

--- Builder_pattern.php
+++ Builder_pattern.php
@@ -35,3 +35,97 @@ class Body {
         return $this->color;
     }
 }
+class Car {
+    private $engine;
+    private $transmission;
+    private $body;
+
+    public function setEngine(Engine $engine) {
+        $this->engine = $engine;
+    }
+
+    public function setTransmission(Transmission $transmission) {
+        $this->transmission = $transmission;
+    }
+
+    public function setBody(Body $body) {
+        $this->body = $body;
+    }
+
+    public function __toString() {
+        return "Car with engine: " . $this->engine->getType() . 
+               ", transmission: " . $this->transmission->getType() . 
+               ", body color: " . $this->body->getColor();
+    }
+}
+
+abstract class CarBuilder {
+    protected $car;
+
+    public function __construct() {
+        $this->car = new Car();
+    }
+
+    abstract public function buildEngine();
+    abstract public function buildTransmission();
+    abstract public function buildBody();
+
+    public function getCar() {
+        return $this->car;
+    }
+}
+
+class SedanBuilder extends CarBuilder {
+    public function buildEngine() {
+        $this->car->setEngine(new Engine("V6"));
+    }
+
+    public function buildTransmission() {
+        $this->car->setTransmission(new Transmission("Automatic"));
+    }
+
+    public function buildBody() {
+        $this->car->setBody(new Body("Red"));
+    }
+}
+
+class SUVBuilder extends CarBuilder {
+    public function buildEngine() {
+        $this->car->setEngine(new Engine("V8"));
+    }
+
+    public function buildTransmission() {
+        $this->car->setTransmission(new Transmission("Manual"));
+    }
+
+    public function buildBody() {
+        $this->car->setBody(new Body("Blue"));
+    }
+}
+
+class CarDirector {
+    private $builder;
+
+    public function __construct(CarBuilder $builder) {
+        $this->builder = $builder;
+    }
+
+    public function constructCar() {
+        $this->builder->buildEngine();
+        $this->builder->buildTransmission();
+        $this->builder->buildBody();
+        return $this->builder->getCar();
+    }
+}
+
+$sedanBuilder = new SedanBuilder();
+$director = new CarDirector($sedanBuilder);
+$sedan = $director->constructCar();
+echo "Создан седан: " . $sedan . "\n";
+
+$suvBuilder = new SUVBuilder();
+$director = new CarDirector($suvBuilder);
+$suv = $director->constructCar();
+echo "Создан внедорожник: " . $suv . "\n";
+
+?>
